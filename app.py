b04f30921dcc6d0857467905e571f29314f4d46c
the-trickster <58772517+the-trickster@users.noreply.github.com>
2021-06-15 18:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,7 @@ def prediction():
     data = request.form.get("first_name")
     print(data)
     prediction = model.predict(data)
-    output = prediction[0]
-    
-    if output == 0:
-        return render_template('index.html',pred='Negative Sentiment')
-    elif ouput == 1:
-        return render_template('index.html',pred='Positive Sentiment')
-    else:
-        return render_template('index.html',pred='Neutral Sentiment')
+    return str(prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
